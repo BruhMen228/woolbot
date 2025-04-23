41c63c32dd56e8f8c0a46e77e7f2c40d2fcb4383
BruhMen <149456401+BruhMen228@users.noreply.github.com>
2025-04-23 10:14:48 +0800

    Update botInit.go
    Update timeout removed

--- botInit.go
+++ botInit.go
@@ -13,7 +13,6 @@ func InitBot(token string, debug bool) (*tgbotapi.BotAPI, tgbotapi.UpdatesChanne
 	bot.Debug = debug
 
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 28800
 	updates := bot.GetUpdatesChan(u)
 
 	return bot, updates, nil
